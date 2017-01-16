9da4b84dd068e2cd25d01ef9931e15fa128978f4
Wedenig Manuel <mwedenig@tsn.at>
2017-01-16 19:56:07 +0100

    Add Currency-Class

---
+++ src/zeldiablo/Currency.java
@@ -0,0 +1,53 @@
+/**
+ * @author Wedenig Manuel
+ */
+package zeldiablo;
+
+public class Currency {
+    
+    //Currency is called silverserpents
+        private int silverserpents;
+       
+	public Currency()
+	{
+	silverserpents = 0;
+	}
+      
+	
+
+        public int getSilverserpents()
+        {
+                //to check out the current amount of silverserpents
+                return silverserpents;
+        }
+
+        public void setValue(int value)
+        {
+                silverserpents = value;
+        }
+
+	public void changeValue(int value)
+	{
+		silverserpents = silverserpents + value;
+	}
+
+
+
+        public void mobdrop(int level)
+        {
+        	int dropvalue;
+        	
+               if (level >= 1)
+               {
+            	   dropvalue = 25 + (level * 5);
+                   silverserpents = silverserpents + dropvalue;
+               }
+         
+               else
+               {
+            	   dropvalue = 0;
+               }
+               
+        }
+    
+}
